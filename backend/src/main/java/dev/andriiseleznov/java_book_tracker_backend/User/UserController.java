392361d580fe2a13399ea255661dbd6a9ddf6c70
Andrii Seleznov <andriiseleznovukr@gmail.com>
2025-01-09 15:43:41 +0100

    mend

--- backend/src/main/java/dev/andriiseleznov/java_book_tracker_backend/User/UserController.java
+++ backend/src/main/java/dev/andriiseleznov/java_book_tracker_backend/User/UserController.java
@@ -24,7 +24,7 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/login")
+    @GetMapping("/info")
     public ResponseEntity<User> loginAndGetUserInfo(@RequestBody LoginRequest loginRequest) {
         Optional<User> user = userService.authenticate(loginRequest.getLogin(), loginRequest.getPassword());
         if (user.isEmpty()) {
